c4e59c1e890fbadadd2dd57b9e2ff5a816036897
Mukul12346 <mukulkumar808182@gmail.com>
2023-12-24 21:03:28 -0800

    Initial Commit

---
+++ professional-5/header.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Demo</title>
+
+    <!-- Stylesheets -->
+    <link href="web_assets/css/bootstrap.css" rel="stylesheet">
+    <link href="plugins/revolution/css/settings.css" rel="stylesheet">
+    <link href="plugins/revolution/css/layers.css" rel="stylesheet">
+    <link href="plugins/revolution/css/navigation.css" rel="stylesheet">
+    <link href="web_assets/css/style.css" rel="stylesheet">
+    <link href="web_assets/css/responsive.css" rel="stylesheet">
+
+    <!-- mobile responsive meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!--Favicon-->
+    <link rel="shortcut icon" href="web_assets/images/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="web_assets/images/favicon.ico" type="image/x-icon">
+    <style>
+        @media only screen and (max-width: 480px) {
+           .dlogo{
+            display:none;
+           }
+           .mlogo{
+            display:block;
+           }
+        }
+        .mlogo{
+            display:none;
+        }
+        html {
+  scroll-behavior: smooth;
+}
+    </style>
+</head>
+
+<body>
+    <div class="page-wrapper">
+<!--End Header Top-->
+
+
+        <!--Main Header-->
+        <header class="main-header sticky-header">
+            <div class="container clearfix">
+                <div class="logo">
+                    <figure>
+                        <a href="index-2.html"><img src="web_assets/images/logo.png" alt="" class="dlogo"></a>
+                    </figure>
+                </div>
+                <div class="header-area clearfix">
+                    <nav class="main-menu">
+                        <div class="navbar-header">
+                            <!-- Toggle Button -->
+                            <button type="button" class="navbar-toggle" data-toggle="collapse"
+                                data-target=".navbar-collapse">
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <div class="logo mlogo" style="margin-left:90px;">
+                                <figure>
+                                    <a href="index-2.html"><img src="web_assets/images/logo.png" alt=""></a>
+                                </figure>
+                            </div>
+                        </div>
+                        <div class="navbar-collapse collapse clearfix">
+                            <ul class="navigation clearfix">
+                                <li class="dropdown current"><a href="index.php">Home</a>                                  
+                                </li>
+                                <li class="dropdown current"><a href="#about">About</a>                                  
+                                </li>
+                                <li class="dropdown current"><a href="#features">features</a>                                  
+                                </li>
+                                <li class="dropdown current"><a href="#services">services</a>                                  
+                                </li>
+                                <li class="dropdown current"><a href="#testimonials">Testimonials</a>                                  
+                                </li>
+                                <li class="dropdown current"><a href="#">Login</a>                                  
+                                </li>
+                                
+                                
+                            </ul>
+                        </div>
+                    </nav>
+                   
+               
+                </div>
+            </div>
+        </header>
+        <!--End Main Header -->
---
+++ professional-6/header.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Demo</title>
+
+    <!-- Favicon -->
+    <link href="web_assets/images/favicon.png" rel="shortcut icon" type="image/png">
+    <link href="web_assets/images/apple-icon.png" rel="icon" type="image/png">
+
+    <!-- Bootstrap CSS -->
+    <link href="web_assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="web_assets/css/style.css" rel="stylesheet">
+
+    <!-- Responsive CSS -->
+    <link href="web_assets/css/responsive.css" rel="stylesheet">
+
+  
+    <style>
+        .pbl{
+            color:black;
+        }
+        html {
+  scroll-behavior: smooth;
+}
+    </style>
+
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+
+
+    <!-- Main Header start -->
+    <header class="main-herader">
+     
+
+        <!-- Header navbar start -->
+        <div class="header-navbar fixed-header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <nav class="navbar navbar-default">
+                            <div class="navbar-header">
+                                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                                    data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                    <span class="sr-only">Toggle navigation</span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
+                                <a class="navbar-brand" href="index-one.html"><img src="web_assets/images/logo.png" alt="">
+                                </a>
+                            </div>
+                            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1"
+                                data-hover="dropdown" data-animations="zoomIn">
+                                <ul class="nav navbar-nav navbar-right">
+                                   
+                                    <li><a href="index.php">Home</a>
+                                   
+                                    <li><a href="#about">About</a>
+                                    <li><a href="#features">Features </a>
+                                    <li><a href="#services">Services </a>
+                                    <li><a href="#testimonials">Testimonials 
+                                                                            </a>
+                                    <li><a href="#">Login</a>
+                                
+                                  
+                                    <li><a href="#">Register</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
